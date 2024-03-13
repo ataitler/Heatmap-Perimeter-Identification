057590486c9fb8bb450297f35f9a46d813fb0687
ataitler <ataitler@gmail.com>
2024-03-13 18:05:44 -0400

    actions input bug fix

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Heatmap-Perimeter-Identification
+
+CC env setup
+1. module load StdEnv/2023
+2. module load opencv/4.9.0
+3. virtualenv --no-download venv_py311
+4. source venv_py311/bin/activate
+5. pip install --no-index --upgrade pip
+6. pip install --no-index numpy
+7. pip install --no-index torch torchvision
+8. pip install --no-index tensorboard
+9. pip install --no-index gymnasium
+
+## TODO
+1. Model save and load
+2. RB save and load
+3. reward plot at the end
+4. Tensorboard
+
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ parser.add_argument('--steps', type=int, default=20)
 parser.add_argument('--updates', type=int, default=5)
 parser.add_argument('--explore', type=int, default=128)
 parser.add_argument('--batch', type=int, default=32)
+parser.add_argument('--verbose', type=bool, default=False)
 args = parser.parse_args()
 
 def main():
@@ -21,11 +22,13 @@ def main():
     num_steps = args.steps
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print('Running on device:', device)
+    if args.verbose:
+        print('Running on device:', device)
+        print('Running for', args.episodes, 'with', args.steps, 'steps each. Batch size:', args.batch)
     transform = transforms.Compose([transforms.ToTensor()])
     env = PIEnv(map="data/rsz_heat_map_with_green.jpg", clean="data/rsz_heat_map.jpg")
 
-    Agent = DQNAgent(action=env.action_space.n, batch_size=args.batch)
+    Agent = DQNAgent(actions=env.action_space.n, batch_size=args.batch)
 
     for i_episode in range(num_episodes):
         state, _ = env.reset(seed=20)
@@ -63,9 +66,11 @@ def main():
         print('Episode', i_episode , 'ended with reward:', total_reward)
         for update in range(n_update_steps):
             # Perform one step of the optimization (on the policy network)
-            print('\tUpdate step', update+1, '...... ', end='')
+            if args.verbose:
+                print('\tUpdate step', update+1, '...... ', end='')
             Agent.optimize_model()
-            print('Done')
+            if args.verbose:
+                print('Done')
 
     # Close the env
     env.close()
